88098c370905074ebac362f8b7ce843daa1c3c4b
Aaron Abrook <aabrook@skedulo.com>
2020-04-23 16:55:07 +1000

    More attempts

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import "./styles.css";
+// import "./styles.css";
+//
+// import { html, define } from "hybrids";
+//
+// export const settings = {
+//   counter: 0
+// };
+//
+// export const render = settings => {
+//   return {
+//     ...settings,
+//     render: ({ counter }) => html`
+//       <div>
+//         My suhweet counter: ${counter}
+//       </div>
+//     `
+//   };
+// };
+//
+// define('counter', render())
 
-import { html, define } from "hybrids";
+import { html, define } from 'hybrids';
 
-export const settings = {
-  counter: 0
+export function increaseCount(host) {
+  host.count += 1;
+}
+
+export const SimpleCounter = {
+  count: 0,
+  render: ({ count }) => html`
+    <button onclick="${increaseCount}">
+      Count: ${count}
+    </button>
+  `,
 };
 
-export const render = settings => {
-  return {
-    ...settings,
-    render: ({ counter }) => html`
-      <div>
-        My suhweet counter: ${counter}
-      </div>
-    `
-  };
-};
-
-define('counter', render())
+define('simple-counter', SimpleCounter);
