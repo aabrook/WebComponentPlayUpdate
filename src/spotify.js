9e46a0b9ad8706965c829dfa4f3a29a68f27435a
Aaron Abrook <aabrook@skedulo.com>
2020-04-23 14:50:18 +1000

    moar

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,13 @@
 import { html, define } from "hybrids";
 
-export const render = settings => {
+export const settings = {
+  source: '',
+  height: 300,
+  width: 380
+};
+
+export const render = ()=> {
+  console.log('rendering the spotify button')
   return {
     ...settings,
     render: ({ source, width, height }) => html`
