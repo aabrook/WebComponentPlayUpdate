77dfbebca6202a3cc397327028f94c92951f39e7
Aaron Abrook <aabrook@skedulo.com>
2020-04-23 17:36:10 +1000

    wip

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 //
 // define('counter', render())
 
-import { html, define } from 'https://unpkg.com/hybrids@4.1.7/src';
+import { html, define } from 'https://unpkg.com/hybrids@4.1.7/esm/index.js';
 
 export function increaseCount(host) {
   host.count += 1;
