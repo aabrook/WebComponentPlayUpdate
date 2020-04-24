b36e8176e47fc8a5fc6837d8f60294d56ad211c9
Aaron Abrook <aabrook@skedulo.com>
2020-04-24 12:43:56 +1000

    Now with friends

---
+++ src/friends-list.js
@@ -0,0 +1,20 @@
+class FriendsList extends HTMLElement {
+  getAttributes() {
+    return this.getAttributeNames().reduce((names, name) => ({ ...names, [name]: this.getAttribute(name) }), {})
+  }
+
+  constructor() {
+    super()
+
+    const { title, friends } = this.getAttributes()
+    var root = this.attachShadow({mode: 'open'});
+    const p = document.createElement('div')
+    p.innerHTML = `
+      <h1>${title || Title}</h1>
+      ${(friends || '').split('\n').map(friend => friend)}
+    `
+    root.appendChild(p);
+  }
+}
+var XComponent = customElements.define('my-heading', Counter);
+
